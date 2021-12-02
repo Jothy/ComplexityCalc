a291e67c6e11e725218ffbd0e098115bf7041403
Jothy Selvaraj <Jothybasu@gmail.com>
2021-12-02 14:23:45 +1100

    PDF2Txt class added in PDF2Txt

--- PDF2Txt.py
+++ PDF2Txt.py
@@ -2,22 +2,27 @@ import PyPDF2
 import numpy as np
 import re
 
-reader=PyPDF2.PdfFileReader("D:\\Projects\\ComplexityCalc\\Docs\\ArcCheck_PDFs\\5.pdf")
-txt=reader.getPage(0).extractText()
-#print(txt)
+class PDF2Txt():
+    def __init__(self):
+        self.Info={}
 
-PatientName=re.search(r'Patient Name : (.*?)Patient ID',txt).group(1)
-PatientID=re.search(r'Patient ID : (.*?)Plan Date',txt).group(1)
-PlanDate=re.search(r'Plan Date : (.*?)SSD',txt).group(1)
-Thresholds=re.search(r' Mode : (.*?)Dose Values',txt).group(1)
-DD=float(Thresholds.split(':')[0])
-DTA=float(Thresholds.split(':')[1])
-DT=float(Thresholds.split(':')[2])
-GammaPass=float(Thresholds.split(':')[6])
+    def ParsePDF(self,filename):
+        reader=PyPDF2.PdfFileReader(filename)
+        txt=reader.getPage(0).extractText()
+        #print(txt)
+        self.Info['PatientName']=re.search(r'Patient Name : (.*?)Patient ID',txt).group(1)
+        self.Info['PatientID']=re.search(r'Patient ID : (.*?)Plan Date',txt).group(1)
+        self.Info['PlanDate']=re.search(r'Plan Date : (.*?)SSD',txt).group(1)
+        Thresholds=re.search(r' Mode : (.*?)Dose Values',txt).group(1)
+        self.Info['DD']=float(Thresholds.split(':')[0])
+        self.Info['DTA'] = float(Thresholds.split(':')[1])
+        self.Info['DT'] = float(Thresholds.split(':')[2])
+        self.Info['GammaPass'] = float(Thresholds.split(':')[6])
+        return self.Info
+
+filename="D:\\Projects\\ComplexityCalc\\Docs\\ArcCheck_PDFs\\5.pdf"
+parser=PDF2Txt()
+info=parser.ParsePDF(filename)
+print(info)
 
 
-
-print('Patient Name: ',PatientName)
-print('Patient ID: ',PatientID)
-print('Plan Date: ',PlanDate)
-print({'Dose Diff':DD,'DTA':DTA,'Threshold':DT,'Gamma': GammaPass})
